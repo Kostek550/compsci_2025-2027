26c3c7de1b8d1edf458c878a59713bddf603fde1
Kostek550 <Kostek.Oscilowicz@gmail.com>
2025-09-16 16:04:03 +0200

    Compete excersises

---
+++ new.folder.py
@@ -0,0 +1,8 @@
+brekkie = input("what did you eat?")  
+print(brekkie) 
+string1="sissy"
+string1.find("s")
+print(string1.find("s"))
+string1="woooooow"
+string1.rfind("w")
+print(string1.rfind("w"))
